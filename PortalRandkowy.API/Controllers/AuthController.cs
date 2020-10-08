513fbc8ad5384308365f3e4cbd485aa3e9fa34f1
Mateusz Świerczyński <swierk94@wp.pl>
2020-10-08 15:38:28 +0200

    first commit

---
+++ PortalRandkowy.API/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Dtos;
+using PortalRandkowy.API.Models;
+
+namespace PortalRandkowy.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repository;
+        public AuthController(IAuthRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register ( UserForRegisterDto userForRegisterDto)
+        {
+
+            userForRegisterDto.Username =  userForRegisterDto.Username.ToLower();
+
+            if (await _repository.UserExists( userForRegisterDto.Username))
+            {
+                return BadRequest("Użytkownik o takiej nazwie już istnieje !");
+            }
+
+            var userToCreate = new User()
+            {
+                Username =  userForRegisterDto.Username
+            };
+
+            var createdUser = await _repository.Register(userToCreate,  userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+
+    }
+}
---
+++ PortalRandkowy.API/Models/User.cs
@@ -0,0 +1,11 @@
+namespace PortalRandkowy.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }    
+        public byte[] PasswordSalt { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+    }
+}

6fc26eb9bb64a28522359fe72511ef3f84ebd9ab
Mateusz Świerczyński <swierk94@wp.pl>
2020-10-11 11:05:44 +0200

    Dodatkowe informacje o użytkowniku

---
+++ PortalRandkowy.API/Models/Photo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace PortalRandkowy.API.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMainPhoto { get; set; }
+
+        
+    }
+}
--- PortalRandkowy.API/Models/User.cs
+++ PortalRandkowy.API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace PortalRandkowy.API.Models
 {
     public class User
@@ -5,7 +8,52 @@ namespace PortalRandkowy.API.Models
         public int Id { get; set; }
         public string Username { get; set; }    
         public byte[] PasswordSalt { get; set; }
-
         public byte[] PasswordHash { get; set; }
+
+        // Informacje o użytkowniku
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ZodiacSign { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        // Informacje na zakładce Info
+        public string Growth { get; set; }
+        public string EyeColor { get; set; }
+        public string HairColor { get; set; }
+        public string MartialStatus { get; set; }
+        public string Education { get; set; }
+        public string Profession { get; set; }
+        public string Children { get; set; }
+        public string Languages { get; set; }
+
+        //Zakładka o mnie
+        public string Motto { get; set; }
+        public string Description { get; set; }
+        public string Personality { get; set; }
+        public string LookingFor { get; set; }
+
+        //Zakładka Pasje, Zainteresowania
+
+        public string Interests { get; set; }
+        public string FreeTime { get; set; }
+        public string Sport  { get; set; }
+        public string Movies { get; set; }
+        public string Music { get; set; }
+
+        // Zakładka preferencje
+
+        public string ILike { get; set; }
+        public string IDoNotLike { get; set; }
+        public string MakesMeLaugh { get; set; }
+        public string ItFeelsBestIn { get; set; }
+        public string FriendWouldDescribeMe { get; set; }
+
+        //Zakładka zdjęcia
+
+        public ICollection<Photo> Photo { get; set; }
+
     }
 }

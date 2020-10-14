08210b52834015e9947b35841d470b9af54bbdb3
Mateusz Świerczyński <swierk94@wp.pl>
2020-10-14 16:40:11 +0200

    Konfiguracja Cloudinary, chmury plików

--- PortalRandkowy.API/Models/Photo.cs
+++ PortalRandkowy.API/Models/Photo.cs
@@ -9,6 +9,7 @@ namespace PortalRandkowy.API.Models
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsMainPhoto { get; set; }   
+        public string public_id {get; set; }
         public User User { get; set; }
         public int UserId { get; set; }     
     }

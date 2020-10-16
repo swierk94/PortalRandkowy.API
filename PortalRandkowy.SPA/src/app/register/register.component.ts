78a7f3d90f31ae31693ed3f9e372effd685d4d5b
Mateusz Świerczyński <swierk94@wp.pl>
2020-10-16 14:35:23 +0200

    FormBuilder Service Validators Formularz rejestracyjny

--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -23,14 +23,17 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit()
   {
-    this.registerForm = new FormGroup(
-      {
-        username: new FormControl('', Validators.required),
-        password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
-        confirmPassword: new FormControl('', Validators.required)
-      },
-        this.passwordMatchValidator
-    );
+    this.createRegisterForm();
+  }
+
+  createRegisterForm()
+  {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+      confirmPassword: ['', Validators.required]
+    },
+    {validator: this.passwordMatchValidator});
   }
 
   passwordMatchValidator(fg: FormGroup)
